ebba201a3133c1944c07807ced4a6ee6c94720d3
Sebastian <votex999@gmail.com>
2019-07-18 22:47:52 +0200

    posts reducer with tests

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import Header from './components/header'
-import Headline from './components/headline'
-import './app.scss'
+import React, { Component } from 'react';
+import Header from './components/header';
+import Headline from './components/headline';
+import './app.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <section className="main">
-        <Headline header="Posts" desc="Click the button to render posts!"/>
-      </section>
-    </div>
-  );
+/* This const is not used within our app.
+   Although we are passing it to the Headline Component
+   it is only here as an exampleof testing PropTypes */
+const tempArr = [{
+  fName: 'Joe',
+  lName: 'Bloggs',
+  email: 'joebloggs@gmail.com',
+  age: 24,
+  onlineStatus: true
+}];
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <section className="main">
+          <Headline header="Posts" desc="Click the button to render posts!" tempArr={tempArr} />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/headline/headline.test.js
+++ src/components/headline/headline.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Headline from './index';
 
-import { findByTestAtrr } from './../../../Utils';
+import { findByTestAtrr, checkProps } from './../../../Utils';
 
 const setUp = (props = {}) => {
   const component = shallow(<Headline {...props} />);
@@ -10,6 +10,28 @@ const setUp = (props = {}) => {
 };
 
 describe('Headline Component', () => {
+
+  describe('Checking PropTypes', () => {
+
+    it('Should not throw a warning', () => {
+
+      const expectedProps = {
+        header: 'Test Header',
+        desc: 'Test Desc',
+        tempArr: [{
+          fName: 'Test fName',
+          lName: 'Test lName',
+          email: 'test@email.com',
+          age: 23,
+          onlineStatus: false
+        }]
+      };
+      const propsErr = checkProps(Headline, expectedProps)
+      expect(propsErr).toBeUndefined();
+
+    });
+
+  });
 
   describe('Have props', () => {
 
@@ -55,4 +77,4 @@ describe('Headline Component', () => {
   });
 
 
-}); 
+});
